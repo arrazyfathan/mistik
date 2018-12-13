dab470eb5582725ea42cc13c058222769ba3acb4
fathan <arrazy.rabbani266@gmail.com>
2018-12-13 11:57:03 +0700

    config login

--- application/controllers/logindb.php
+++ application/controllers/logindb.php
@@ -29,7 +29,8 @@ class logindb extends CI_Controller{
                 $this->session->set_userdata('masuk',TRUE);
                 $this->session->set_userdata('akses','2');
                 $this->session->set_userdata('ses_id',$data['id']);
-                $this->session->set_userdata('ses_nama',$data['nama']);
+                $this->session->set_userdata('ses_nama',$data['nama_depan']);
+                $this->session->set_userdata('ses_nama2',$data['nama_belakang']);
                 redirect('petugas');
             }else {
                 $url=base_url('logindb');
--- application/models/logindb_model.php
+++ application/models/logindb_model.php
@@ -2,13 +2,13 @@
 class logindb_model extends CI_Model{
     //cek nip dan password petugas
     function auth_petugas($username,$password){
-        $query=$this->db->query("SELECT * FROM petugas WHERE id_petugas='$username' AND pass=MD5('$password') LIMIT 1");
+        $query=$this->db->query("SELECT * FROM petugas WHERE username='$username' AND password=MD5('$password') LIMIT 1");
         return $query;
     }
  
     //cek nim dan password bdpb
     function auth_bdpb($username,$password){
-        $query=$this->db->query("SELECT * FROM bdpb WHERE id='$username' AND pass=MD5('$password') LIMIT 1");
+        $query=$this->db->query("SELECT * FROM bdpb WHERE nama='$username' AND password=MD5('$password') LIMIT 1");
         return $query;
     }
  
